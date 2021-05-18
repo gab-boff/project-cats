1e10a25c5b9b09dd85f435c4e62ead86aa50ae50
gab-boff <gmeninboff@gmail.com>
2021-05-17 21:32:42 -0300

    Cats faltando css

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 async function getCatsPics() {
   fetch('https://thatcopy.pw/catapi/rest/')
-  .then((cats) => cats.json())
-  .then((pics) => {
-    const thePic = document.querySelector('.pic-cat');
-    thePic.setAttribute('src', pics.url)
-  })
+    .then((cats) => cats.json())
+    .then((pics) => {
+      const thePic = document.querySelector('.pic-cat');
+      thePic.setAttribute('src', pics.url)
+    })
 }
 
 getCatsPics();
 
+const myDiv = document.querySelector('#facts');
+
 function getCatsFacts() {
   fetch('https://cat-fact.herokuapp.com/facts')
-  .then((cats) => cats.json())
-  .then((catsFacts) => catsFacts.forEach((facts) => document.body.innerHTML += `<p>${facts.text}</p>`) )
+    .then((cats) => cats.json())
+    .then((catsFacts) => catsFacts.forEach((facts) => myDiv.innerHTML += `<p><br>${facts.text}</p><br>`))
 }
 
 getCatsFacts();
@@ -20,5 +22,9 @@ getCatsFacts();
 function inputClick() {
   const catValue = document.querySelector('#http-cat');
   const thePic = document.querySelector('.pic-cat');
-  thePic.setAttribute('src', `https://http.cat/${catValue.value}`)
+  if (catValue.value === '') {
+    thePic.setAttribute('src', 'https://http.cat/404')
+  } else {
+    thePic.setAttribute('src', `https://http.cat/${catValue.value}`)
+  }
 }
